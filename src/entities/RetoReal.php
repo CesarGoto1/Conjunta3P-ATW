2499572972a1d35e03b3dd79baf46df6b4e56682
David11tene <drteneguznay@espe.edu.ec>
2025-08-14 17:32:59 -0500

    Entidades Retos

---
+++ src/entities/RetoReal.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+namespace App\Entities;
+
+class RetoReal extends RetoSolucionable
+{
+    private string $entidad_colaboradora;
+
+    public function __construct(?int $id = null, ?string $descripcion = null, ?string $entidad_colaboradora = null)
+    {
+        parent::__construct($id, $descripcion);
+        $this->entidad_colaboradora = $entidad_colaboradora;
+    }
+
+    public function getEntidadColaboradora(): ?string
+    {
+        return $this->entidad_colaboradora;
+    }
+
+    public function setEntidadColaboradora(string $entidad_colaboradora): void
+    {
+        $this->entidad_colaboradora = $entidad_colaboradora;
+    }
+
+}
---
+++ src/entities/RetoSolucionable.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+namespace App\Entities;
+
+abstract class RetoSolucionable
+{
+    protected ?int $id;
+    protected ?string $descripcion;
+
+    public function __construct(?int $id = null, ?string $descripcion = null)
+    {
+        $this->id = $id;
+        $this->descripcion = $descripcion;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDescripcion(): ?string
+    {
+        return $this->descripcion;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function setDescripcion(string $descripcion): void
+    {
+        $this->descripcion = $descripcion;
+    }
+
+
+}
